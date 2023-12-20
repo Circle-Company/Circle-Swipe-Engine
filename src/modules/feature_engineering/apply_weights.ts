e796085301f5ac7a71ab8c7d118eee08d5520be4
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2023-12-20 13:09:13 -0300

    add SyntacticAnalyser

--- src/modules/feature_engineering/apply_weights.ts
+++ src/modules/feature_engineering/apply_weights.ts
@@ -11,16 +11,16 @@ export function applyInteractionWeights({
     const actionWeightsData = require('../../database/action_weights.json')
     const actionWeights: actionWeightsProps = actionWeightsData
 
-    const like = Number( sigmoid( booleanToNumber(interactions.like) * actionWeights.like.weight ).toFixed(4))
-    const share = Number(sigmoid( booleanToNumber(interactions.share) * actionWeights.share.weight ).toFixed(4))
-    const click_into_moment = Number(sigmoid( booleanToNumber(interactions.click_into_moment) * actionWeights.click_into_moment.weight ).toFixed(4))
-    const watch_time =  Number(sigmoid( interactions.watch_time * actionWeights.watch_time.weight ).toFixed(4))
-    const click_profile = Number(sigmoid( booleanToNumber(interactions.click_profile) * actionWeights.click_profile.weight ).toFixed(4))
-    const comment = Number(sigmoid( booleanToNumber(interactions.comment) * actionWeights.comment.weight ).toFixed(4))
-    const like_comment = Number(sigmoid( booleanToNumber(interactions.like_comment) * actionWeights.like_comment.weight ).toFixed(4))
-    const pass_to_next = Number(sigmoid( booleanToNumber(interactions.pass_to_next) * actionWeights.pass_to_next.weight ).toFixed(4))
-    const show_less_often = Number(sigmoid( booleanToNumber(interactions.show_less_often) * actionWeights.show_less_often.weight ).toFixed(4))
-    const report = Number(sigmoid( booleanToNumber(interactions.report) * actionWeights.report.weight ).toFixed(4))
+    const like = booleanToNumber(interactions.like) * actionWeights.like.weight
+    const share = booleanToNumber(interactions.share) * actionWeights.share.weight
+    const click_into_moment = booleanToNumber(interactions.click_into_moment) * actionWeights.click_into_moment.weight
+    const watch_time =  interactions.watch_time * actionWeights.watch_time.weight
+    const click_profile = booleanToNumber(interactions.click_profile) * actionWeights.click_profile.weight
+    const comment = booleanToNumber(interactions.comment) * actionWeights.comment.weight
+    const like_comment = booleanToNumber(interactions.like_comment) * actionWeights.like_comment.weight
+    const pass_to_next = booleanToNumber(interactions.pass_to_next) * actionWeights.pass_to_next.weight
+    const show_less_often = booleanToNumber(interactions.show_less_often) * actionWeights.show_less_often.weight
+    const report = booleanToNumber(interactions.report) * actionWeights.report.weight
 
     return {
         like,
--- src/modules/feature_engineering/index.ts
+++ src/modules/feature_engineering/index.ts
@@ -1,13 +1,31 @@
 import { featureEngineeringProps } from './types'
 import { applyInteractionWeights } from './apply_weights'
 import objectToCSV from '../../middlewares/obj_to_csv'
+import syntacticAnalyser from './syntactic_analyser'
 
 export function Feature_Engineering({
-    userInteractions
+    userInteractions,
 }: featureEngineeringProps) {
 
     const matrix = userInteractions.map((i, index) => {
-        return applyInteractionWeights({ interactions: i.interaction})
+
+        const interaction = applyInteractionWeights({ interactions: i.interaction})
+
+        const tags = i.informations.tags.map((tag, index) => {
+            return syntacticAnalyser({word: tag})
+        })
+
+        return {
+            interaction,
+            informations: {
+                tags: tags,
+                duration: i.informations.duration,
+                type: i.informations.type == "video"? 1 : 0,
+                language : i.informations.language == 'en'? 0 : 1,
+                country: i.informations.country == 'US'? 0 : 1
+
+            }
+        }
     })
     return matrix
 }
