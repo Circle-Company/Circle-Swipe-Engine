1a86adeaae4aa771f0f7eff58ff4fc332021c080
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-03-28 14:24:51 -0300

    add algorithms on model controller

--- src/modules/modules-controller.ts
+++ src/modules/modules-controller.ts
@@ -1,8 +1,14 @@
-import { Feature_Engineering } from './feature_engineering'
+import { pre_processing } from './pre-processing'
+import { interaction_rate } from './interaction_rate'
+import { InteractionQueueProps } from './types'
+import { calculeTagsWeight } from './tags_weight'
 
-export function Modules_Controller(user_interactions:any) {
-
-    return Feature_Engineering({
-        userInteractions: user_interactions
-    })
+type ModuleControllerProps = {
+    interaction_queue: InteractionQueueProps
+}
+export async function Modules_Controller({interaction_queue}:ModuleControllerProps) {
+    const processed_interactions = await pre_processing(interaction_queue)
+    const aditional_features = await interaction_rate(processed_interactions)
+    const tags_with_weights = await calculeTagsWeight(processed_interactions, aditional_features)
+    return tags_with_weights
 }
