13edd41014ea0b39b4470236fc7f03af1fecc433
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-04-22 19:10:56 -0300

    add store interaction

--- src/connection/controllers/controller-swipe_engine.ts
+++ src/connection/controllers/controller-swipe_engine.ts
@@ -1,12 +1,50 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import {Modules_Controller} from '../../modules/modules-controller'
+import MomentInteraction from '../../models/moments/moment_interaction-model.js'
+import { InternalServerError } from '../../errors'
+import { normalizeWatchTime } from '../../modules/pre-processing'
+import { calcule_one_interaction_rate, interaction_rate } from '../../modules/interaction_rate'
 
-export async function getMoments(req:Request, res:Response) {
+
+export async function getMoments(req:Request, res:Response){
     const data = await Modules_Controller(req.body)
     return res.status(StatusCodes.ACCEPTED).json(data)
 }
 
+export async function storeMomentInteraction(req: Request, res: Response){
+    const {user_id, moment_owner_id, moment_id, interaction} = req.body
+    try{
+        console.log(interaction)
+
+        const processed_interaction = {
+            like:interaction.like? 1: 0,
+            share: interaction.share? 1: 0,
+            click_into_moment: interaction.click_into_moment? 1: 0,
+            watch_time: normalizeWatchTime(interaction.watch_time, 0) /1000,
+            click_profile: interaction.click_profile? 1: 0,
+            comment: interaction.comment? 1: 0,
+            like_comment: interaction.like_comment? 1: 0,
+            pass_to_next: interaction.pass_to_next? 1: 0,
+            show_less_often: interaction.show_less_often? 1: 0,
+            report: interaction.report? 1: 0
+        }
+        const stored_interaction = await MomentInteraction.create({
+            user_id,
+            moment_owner_id,
+            moment_id,
+            ...interaction,
+            interaction_rate: calcule_one_interaction_rate(processed_interaction),
+            created_at: new Date(),
+            updated_at: new Date()
+        })
+        res.json(stored_interaction).send()
+    } catch(err: any) {
+        throw new InternalServerError({ message: err.message})
+    }
+}
+
 export const SwipeEngine = {
-    getMoments: getMoments
+    getMoments,
+    storeMomentInteraction
 }
