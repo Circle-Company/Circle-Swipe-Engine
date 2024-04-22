9dc8c227764a62492b71bc3f395c6b7180077c89
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-04-22 19:18:21 -0300

    add calculated similarities

--- src/modules/modules-controller.ts
+++ src/modules/modules-controller.ts
@@ -1,14 +1,16 @@
 import { pre_processing } from './pre-processing'
 import { interaction_rate } from './interaction_rate'
-import { InteractionQueueProps } from './types'
+import { InteractionQueueProps } from './types' 
 import { calculeTagsWeight } from './tags_weight'
+import calculate_similarities from './calculate_similarities'
 
-type ModuleControllerProps = {
+type ModuleControllerProps = { 
     interaction_queue: InteractionQueueProps
 }
 export async function Modules_Controller({interaction_queue}:ModuleControllerProps) {
+    const calculated_similarities = await calculate_similarities()
     const processed_interactions = await pre_processing(interaction_queue)
     const aditional_features = await interaction_rate(processed_interactions)
     const tags_with_weights = await calculeTagsWeight(processed_interactions, aditional_features)
-    return tags_with_weights
+    return {calculated_similarities, processed_interactions, aditional_features, tags_with_weights}
 }
