14c690900a794f13e3baa17122521bc7af30e92b
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-18 10:03:15 -0300

    disable similarity matrix formater

--- src/modules/calculate_similarities/moment_x_moment/index.ts
+++ src/modules/calculate_similarities/moment_x_moment/index.ts
@@ -1,6 +1,6 @@
 import find_posts from "./find_posts"
 import { mapSimilaritiesToMoments } from "./format_similarities"
 export default async function moment_x_moment(){
-   const [similarityMatrix, similrityIndex] = await find_posts()
-   return  mapSimilaritiesToMoments(similarityMatrix, similrityIndex)
+   return await find_posts()
+   //return  mapSimilaritiesToMoments(similarityMatrix, similrityIndex)
 }
