a63400c31ea6703b051c460ecf377d8eefeb6489
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-18 10:05:45 -0300

    add interaction_tags_algorithm + negative_content_algorithm modules

--- src/modules/modules-controller.ts
+++ src/modules/modules-controller.ts
@@ -3,6 +3,8 @@ import { positive_interaction_rate } from './positive_interaction_rate'
 import { InteractionQueueProps } from './types' 
 import { calculeTagsWeight } from './tags_weight'
 import calculate_similarities from './calculate_similarities'
+import interaction_tags_algorithm from './interaction_tags_algorithm'
+import negative_content_algorithm from './negative_content_algorithm'
 
 type ModuleControllerProps = { 
     interaction_queue: InteractionQueueProps
@@ -12,5 +14,16 @@ export async function Modules_Controller({interaction_queue}:ModuleControllerPro
     const processed_interactions = await pre_processing(interaction_queue)
     const aditional_features = await positive_interaction_rate(processed_interactions)
     const tags_with_weights = await calculeTagsWeight(processed_interactions, aditional_features)
-    return {calculated_similarities, processed_interactions, aditional_features, tags_with_weights}
+    const posts_from_tags = await interaction_tags_algorithm({
+        tags_with_weights,
+        users_similarity: calculated_similarities.users_similarity,
+        interaction_queue
+    })
+
+    const negative_post = await negative_content_algorithm({
+        users_similarity: calculated_similarities.users_similarity,
+        interaction_queue
+    }) 
+    const posts_arr = [posts_from_tags, negative_post]
+    return posts_arr
 }
