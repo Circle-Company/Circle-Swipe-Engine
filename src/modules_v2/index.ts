1e8001c6f110d898943fb8c1918926a512e2ba16
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-10-04 09:08:49 -0300

    add: recommendation engine v2

---
+++ src/modules_v2/index.ts
@@ -0,0 +1,16 @@
+export async function ModuleV2() {
+    /**
+    posts: 
+    - embedding de post: 
+    - - texto
+    - - tags
+    - - engajamento 
+    */
+    /**
+    functions: 
+    - criar ou pegar a embedding do usuario
+    - pegar similaridade entre posts das ultimas 48 horas, se tive menos que 100 pegar os ultimos 100
+    - 
+    - get top 10 posts to send to user
+    */
+}
